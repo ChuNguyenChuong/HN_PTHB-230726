fddb6821859b65878b95d181c90fbc2094e094c0
cnchuongvenn <chunguyenchuong2014bg@gmail.com>
2023-08-15 17:40:34 +0700

    lesstion 8

---
+++ 8.JS base/app.js
@@ -0,0 +1,122 @@
+// var age;  // undefine
+
+// hien thi thong tin ow trinh duyet
+// console.log("hi")
+// console.error("loi 404")
+// console.warn("canh bao  loi")
+// console.info("log infor")
+
+// console.table("canh bao  loi")
+
+// console.log(age);
+// console.log(notification);
+
+// var age = 18;
+// age = 15
+
+
+
+// let notification = "canh bao  loi"
+// const isRunning = false
+
+
+// if (1 < 2) {
+//   // block scope
+//   // var name = "cnchuong"
+
+// }
+// global scope
+// console.log(name);
+
+
+// tu khoa khai bao bien : var - let const
+
+// console.log(notification);
+
+// var age = 20
+// let notification = "check changed"
+
+// notification = "changed"
+// age = 20
+// isRunning = true
+
+
+// data type
+
+
+// let age = 16 // number
+// let name = "cnchuong" // ""  '' ``
+// let name2 = 'venn';
+// let name3 = `chu nguyen chuong - ${name}`
+// console.log(name3);
+
+
+// // let name12 = name + " " + name2 //  "cnchuong" + " " + "venn"
+// // console.log(name12);
+
+
+// let isRunning = true // true / false
+// let pet;  // undefine
+// let nullValue = null;
+
+// let Person = {
+//   name: "chuong",
+//   age: 19
+// } // object
+
+// let petList = ["cat", "dog"] // array
+
+// typeof : check kiểu dữ liệu
+// console.log(typeof petList);
+
+// let name = "cnchuong"
+
+// alert(name)
+// let valueConfirm = confirm("ban co chac chan xoa ban ghi nay khong ?")
+
+// console.log(valueConfirm);
+
+// let valueUserEnter = prompt("de nghi nhap so dien thoai lien lac")
+
+// console.log(valueUserEnter);
+
+
+// const nodeName = document.getElementById("name")
+
+// console.log(nodeName);
+
+
+/// so ngay nhien
+// let pi = 3.14589794564634234;
+
+// let number = 10;
+
+// console.log(++number);
+
+// console.log(number);
+
+// let sum = number1 + number2
+
+
+
+// gan
+// let number 
+// number = 10
+let number = 10;
+let nextNumber = 20;
+
+console.log(sum);
+
+var sum = number + nextNumber;
+
+// về nhà tìm hiểu : truthy / falsy
+
+
+// let 
+// const
+
+/// dong 3000 
+
+let age = 165;
+
+age = 195
